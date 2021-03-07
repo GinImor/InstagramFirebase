4b35217b5f61bdeb1c5b33ba7b3933883ca75b7c
Gin Imor <openheart23@163.com>
2021-03-07 22:22:07 +0800

    Initial set up of sign up page

---
+++ InstagramFirebase/Extensions/UIView+layout.swift
@@ -0,0 +1,64 @@
+//
+//  UIView+tAMIC.swift
+//  IntermediateTraining
+//
+//  Created by Gin Imor on 1/31/21.
+//  Copyright © 2021 Brevity. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+  
+  var tAMIC: Bool {
+    get { translatesAutoresizingMaskIntoConstraints }
+    set { translatesAutoresizingMaskIntoConstraints = newValue }
+  }
+  
+  func disableTAMIC() {
+    tAMIC = false
+  }
+  
+  public static let defaultPadding: CGFloat = 8.0
+
+  /// Creates a new instance of the receiver class, configured for use with Auto Layout.
+  /// - Returns: An instance of the receiver class.
+  public static func autolayoutNew() -> Self {
+    let view = self.init(frame: .zero)
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }
+  
+  func pinToSuperviewEdges() {
+    guard let superview = superview else { return }
+    disableTAMIC()
+    NSLayoutConstraint.activate([
+      topAnchor.constraint(equalTo: superview.topAnchor),
+      leadingAnchor.constraint(equalTo: superview.leadingAnchor),
+      trailingAnchor.constraint(equalTo: superview.trailingAnchor),
+      bottomAnchor.constraint(equalTo: superview.bottomAnchor)
+    ])
+  }
+  
+  func centerToSuperviewSafeAreaLayoutGuide() {
+    guard let superview = superview else { return }
+    disableTAMIC()
+    NSLayoutConstraint.activate([
+      centerXAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.centerXAnchor),
+      centerYAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.centerYAnchor)
+    ])
+  }
+  
+  func centerToSuperviewSafeAreaLayoutGuide(superview: UIView) {
+    superview.addSubview(self)
+    centerToSuperviewSafeAreaLayoutGuide()
+  }
+  
+  func pinToSuperviewHorizontalEdges(defaultSpacing: CGFloat = 0, trailingSpacing: CGFloat? = nil) {
+    guard let superview = superview else { return }
+    NSLayoutConstraint.activate([
+        leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: defaultSpacing),
+        trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: -(trailingSpacing ?? defaultSpacing))
+    ])
+  }
+}
--- InstagramFirebase/ViewController.swift
+++ InstagramFirebase/ViewController.swift
@@ -10,12 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  static func textField(placeholder: String) -> UITextField {
+    let textField = UITextField()
+    textField.placeholder = placeholder
+    textField.borderStyle = .roundedRect
+    textField.font = UIFont.preferredFont(forTextStyle: .body)
+    textField.backgroundColor = UIColor(white: 0.0, alpha: 0.03)
+    return textField
+  }
+  
+  var avatarButton: UIButton = {
+    let button = UIButton(type: .system)
+    button.imageView?.contentMode = .scaleAspectFit
+    button.setImage(UIImage(named: "plus_photo"), for: .normal)
+    return button
+  }()
+  var emailTextField = ViewController.textField(placeholder: "Email")
+  var userNameTextField = ViewController.textField(placeholder: "Username")
+  var passwordTextField: UITextField = {
+    let textField = ViewController.textField(placeholder: "Password")
+    textField.isSecureTextEntry = true
+    return textField
+  }()
+  var signUpButton: UIButton = {
+    let button = UIButton(type: .system)
+    button.setTitle("Sign Up", for: .normal)
+    button.backgroundColor = UIColor(rgb: (149, 204, 244))
+    button.layer.cornerRadius = 5
+    button.tintColor = .white
+    return button
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    
+    setupViews()
   }
   
-  
+  func setupViews() {
+    view.backgroundColor = .systemBackground
+    let inputStackView = UIStackView.verticalStack(arrangedSubviews: [emailTextField, userNameTextField, passwordTextField, signUpButton])
+    let signUpStackView = UIStackView.verticalStack(arrangedSubviews: [avatarButton, inputStackView])
+    
+    inputStackView.spacing = 2 * UIView.defaultPadding
+    signUpStackView.spacing = 3 * UIView.defaultPadding
+    view.addSubview(signUpStackView)
+    NSLayoutConstraint.activate([
+      signUpStackView.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 4.0),
+      signUpStackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.safeAreaLayoutGuide.leadingAnchor, multiplier: 4.0),
+      view.safeAreaLayoutGuide.trailingAnchor.constraint(equalToSystemSpacingAfter: signUpStackView.trailingAnchor, multiplier: 4.0),])
+  }
 
 
 }
