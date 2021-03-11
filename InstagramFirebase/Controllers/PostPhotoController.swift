63b67f2fcdad32060990a34d5001dcbc782ab128
Gin Imor <openheart23@163.com>
2021-03-11 11:43:15 +0800

    Upload post to Firebase

--- InstagramFirebase/Controllers/PostPhotoController.swift
+++ InstagramFirebase/Controllers/PostPhotoController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Photos
+import Firebase
 
 class PostPhotoController: UIViewController {
   
@@ -37,7 +38,12 @@ class PostPhotoController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    setupNavigationBar()
     setupViews()
+  }
+  
+  private func setupNavigationBar() {
+    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Post", style: .plain, target: self, action: #selector(post))
   }
   
   private func setupViews() {
@@ -66,4 +72,57 @@ class PostPhotoController: UIViewController {
     ])
     contentStackView.pinToSuperviewEdges()
   }
+  
+  @objc func post() {
+    guard textView.text ?? "" != "",
+      let postImage = imageView.image,
+      let postImageData = postImage.jpegData(compressionQuality: 0.5) else { return }
+    navigationItem.rightBarButtonItem?.isEnabled = false
+    let uuid = NSUUID().uuidString
+    let storageRef = Storage.storage().reference().child("post_images/\(uuid)")
+    storageRef.putData(postImageData, metadata: nil) {[weak self] (metaData, error) in
+      guard error == nil else {
+        print("put data error: \(String(describing: error))")
+        self?.navigationItem.rightBarButtonItem?.isEnabled = true
+        return
+      }
+      
+      storageRef.downloadURL { (url, error) in
+        guard error == nil else {
+          self?.navigationItem.rightBarButtonItem?.isEnabled = true
+          print("fetch download url error: \(String(describing: error))")
+          return
+        }
+        guard let imageUrl = url?.absoluteString else {
+          self?.navigationItem.rightBarButtonItem?.isEnabled = true
+          print("image url == nil")
+          return
+        }
+        self?.savePostMetaDataWithImageURL(imageUrl)
+      }
+    }
+  }
+  
+  private func savePostMetaDataWithImageURL(_ imageUrl: String) {
+    guard let uid = Auth.auth().currentUser?.uid,
+      let caption = textView.text,
+      let image = imageView.image else { return }
+    let postMetaData: [String: Any] = [
+      "imageUrl": imageUrl,
+      "caption": caption,
+      "creationData": Date().timeIntervalSince1970,
+      "imageWidth": image.size.width,
+      "imageHeight": image.size.height
+      ]
+    let postRef = Database.database().reference().child("Posts/\(uid)").childByAutoId()
+    postRef.updateChildValues(postMetaData) {[weak self] (error, ref) in
+      guard error == nil else {
+        self?.navigationItem.rightBarButtonItem?.isEnabled = true
+        print("updata post database")
+        return
+      }
+      self?.presentingViewController?.dismiss(animated: true)
+    }
+  }
+  
 }
